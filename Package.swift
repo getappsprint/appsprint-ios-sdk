f29e883305ff9a9dbff594557c1c15be4fbfc677
dev-err418 <arthurs.dev@gmail.com>
2026-05-11 14:33:05 +0200

    Release iOS SDK v1.0.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppSprintSDK",
-            url: "https://github.com/getappsprint/appsprint-ios-sdk/releases/download/0.3.0/AppSprintSDK.xcframework.zip",
+            url: "https://github.com/getappsprint/appsprint-ios-sdk/releases/download/v1.0.0/AppSprintSDK.xcframework.zip",
             checksum: "6f76d784566a4f9fdd6cced4a5be84dd645004e8d2d62e6f3f9e61c2bde65f1b"
         )
     ]
